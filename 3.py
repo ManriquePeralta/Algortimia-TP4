d7f4e616a45ee55e015421142542b73656c0aa61
ManriquePeralta <lukimanri1@gmail.com>
2025-04-16 14:18:05 -0300

    clase

--- 3.py
+++ 3.py
@@ -1,13 +1,13 @@
-num = int(input("Ingrese un número (-1 para finalizar): "))
-menor = num
-mayor = num
+temp = int(input("Ingrese la temperatura: "))
+menor = temp
+mayor = temp
 
-while num != -1:
-    if num < menor:
-        menor = num
-    if num > mayor:
-        mayor = num
-    num = int(input("Ingrese un número (-1 para finalizar): "))
+while temp != 0:
+    if temp < menor:
+        menor = temp
+    if temp > mayor:
+        mayor = temp
+    temp = int(input("Ingrese un número (-1 para finalizar): "))
 
 print("El menor valor ingresado fue:", menor)
 print("El mayor valor ingresado fue:", mayor)
---
+++ clase temp.py
@@ -0,0 +1,66 @@
+"""Se ingresan temperaturas, la carga finaliza con un valor de temperatura igual a 0.
+Informar
+1) Temperatura maxima, y posicion respecto del ingreso
+2) Temperatura minima
+3) Porcentaje de temperaturas menores a cero.
+4) Promedio de las temperaturas positivas"""
+
+num = int(input("Ingrese una temperatura (0 para finalizar): "))
+
+menor = num
+
+mayor = num
+pos_mayor = 1
+posicion = 1
+
+contador = 0
+contador_negativas = 0
+
+suma_positivas = 0
+contador_positivas = 0
+
+
+
+while num != 0:
+    # Actualizo el valor mas bajo en cada input
+    if num < menor:
+        menor = num
+
+    # Actualizo el valor mas alto en cada input nuevo y su posición
+    if num > mayor:
+        mayor = num
+        pos_mayor = posicion
+
+    # Contamos temperaturas menores a cero
+    if num < 0:
+        contador_negativas += 1
+
+    # Sumar temperaturas positivas y contarlas
+    if num > 0:
+        contador_positivas += 1
+        suma_positivas += num #Para luego poder calcular el promedio
+
+    # En cada iteracion incremento el contador total y la posición para cada input
+    contador += 1
+    posicion += 1
+
+    # Pedimos la nueva temperatura para tener un num actualizado y rehacer todo el proceso del bucle
+    num = int(input("Ingrese una temperatura (0 para finalizar): "))
+
+# Calcular el porcentaje de temperaturas menores a cero
+if contador > 0:
+    porcentaje_negativas = (contador_negativas / contador) * 100
+else:
+    porcentaje_negativas = 0
+
+# Calcular el promedio de temperaturas positivas
+if contador_positivas > 0:
+    promedio_positivas = suma_positivas / contador_positivas
+else:
+    promedio_positivas = 0
+
+# Mostrar los resultados
+print("El mayor valor ingresado fue:", mayor, "en la posición:", pos_mayor)
+print("El menor valor ingresado fue:", menor)
+print("El porcentaje de temperaturas menores a cero es:", porcentaje_negativas, "%")
+print("El promedio de las temperaturas positivas es:", promedio_positivas)
